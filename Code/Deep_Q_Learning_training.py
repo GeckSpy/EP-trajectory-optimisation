e1f1f44ef05a7997d51323cc22324950ef4b89b0
mathieulngt <mathieu.longatte@hotmail.fr>
2024-12-10 16:54:25 +0100

    clean Deep Q learning

--- Code/Deep_Q_Learning_training.py
+++ Code/Deep_Q_Learning_training.py
@@ -21,7 +21,7 @@ import gymnasium
 # For time limit
 import time
 
-# structure to save transitions 
+# Structure to save transitions 
 from collections import namedtuple , deque
 Transition = namedtuple("Transition",["state","action","next_state","reward"])
 
@@ -44,17 +44,21 @@ class ReplayMemory():
         self.memory_ = deque(maxlen=maxlen)
 
     def push(self,x : Transition):
+        """Add a new transition to the memory"""
         self.memory_.append(x)
 
     def sample(self,batch_size : int) -> list[Transition]:
+        """Sample a subset of size batch_size uniformly among the subsets of size batch_size of the memory. 
+           We assume that such subset exists.
+        """
         return rd.sample(self.memory_,batch_size)
     
     def clear(self):
+        """Clear the memory"""
         return self.memory_.clear()
     
     def __len__(self):
         return len(self.memory_)
-
 
 class DQN(nn.Module):
     def __init__(self,layer_size,state_size,action_n):
@@ -76,15 +80,14 @@ class DQN(nn.Module):
         return self.network(x)
     
     def save(self,filename : str = None):
-        """save model's parameters in the given file"""
+        """Save model's parameters in the given file"""
         if (filename == None):
             filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         torch.save(self.state_dict(),filename)
 
     def load(self,filename : str):
-        """load parameters stored in the given file"""
+        """Load parameters stored in the given file"""
         self.load_state_dict(torch.load(filename, weights_only=True))
-
 
 class Env():
     def __init__(self):
@@ -108,7 +111,7 @@ class Env():
         self.track_for_training = int(len(TRACKS)*0.8)
 
     def state(self):
-        """ On définit un état comme étant un batch de taille 1 ou None"""
+        """A state is either a batch of size 1 or None"""
         if (self.state_gym == None or self.done) :
             return None
         else :
@@ -117,6 +120,7 @@ class Env():
             return torch.tensor([arr],dtype=torch.float)
 
     def show_state(self):
+        """plot the current trajectory"""
         self.env.render(show_trajectory=True)
 
     def reset(self):
@@ -124,25 +128,15 @@ class Env():
         rd_track = rd.randint(0, self.track_for_training -1)
         self.state_gym , _ = self.env.reset(TRACKS[rd_track])
         self.done = False
-    
-    def dist(state):
-        """Calcule la longueur d'un plus court chemin entre state et goal (sous forme d'un flotant)"""
-        goal = torch.tensor([[11,3]],dtype=torch.float)
-        start = torch.tensor([[0,3]],dtype=torch.float)
-        if (torch.equal(state,start)):
-           return torch.tensor(13,dtype=torch.float)
-        else :
-           return torch.sum(torch.abs(state-goal))
        
     def step(self,action : torch.tensor) :
-        """ Fais un pas depuis l'état actuel via l'action donnée et renvoit la transition observéex
-            Une action est un tenseur contenant un seul scalaire """
+        """Move from the current state to the next state thanks to the action, observing a reward"""
         if (self.done):
             raise(ValueError("Trying to move from a final state"))
 
         prev_state = self.state()
 
-        # do the step and update the new gym state
+        # Do the step and update the new gym state
         acc, turn = self.env.int_to_action(action.item())
         if np.absolute(turn) > self.env.max_turn:
             print(action.item(), (acc, turn))
@@ -154,6 +148,7 @@ class Env():
 
         reward_normalizer = self.env.reward_max
         qtable_normalizer = 1/(1-self.discount_factor)
+        # We normalize the reward
         reward = torch.tensor(reward/(reward_normalizer*qtable_normalizer), dtype=torch.float).reshape((1,1))
         action = torch.tensor(action.item()).reshape((1,1))
 
@@ -161,17 +156,21 @@ class Env():
         return transition
     
     def policy(self):
+        """Predict the best action according to our current model"""
         if (self.done):
             raise(ValueError("Trying to predict a move from a final state"))
         return self.model(self.state()).max(1).indices.reshape((1,1))
     
     def random_action(self) -> torch.tensor :
+        """Select a random action"""
         if (self.done):
             raise(ValueError("Trying to sample a move from a final state"))
         action = rd.randint(0,self.n_action-1)
         return torch.tensor(action).reshape((1,1))
 
 def optimize(env : Env,optimizer,criterion,batch_size,discount_factor):
+    """Optimize the model according to batch_size random transitions observed in the past"""
+    # We optimize only if have >= batch_size transition in our replay buffer
     if (len(env.memory) < batch_size) :
         return 
 
@@ -181,7 +180,8 @@ def optimize(env : Env,optimizer,criterion,batch_size,discount_factor):
     # A tuple with four coordinates : 
     # state -> a batch of size batch_size of states 
     # action -> a batch of size batch_size of actions
-    # ect
+    # reward -> a batch of size batch_size of rewards
+    # next_state -> a batch of size batch_size of the next states
     batch = Transition(*zip(*transition))
 
     state_batch = torch.cat(batch.state)
@@ -204,21 +204,23 @@ def optimize(env : Env,optimizer,criterion,batch_size,discount_factor):
     optimizer.zero_grad()
     loss = criterion(predicted,expected)
     loss.backward()
+    # We bound the gradient by 100
     torch.nn.utils.clip_grad_value_(env.model.parameters(), 100)
     optimizer.step()
 
-
 def measure_policy_time(env) :
+    """Measure the time needed by the model to predict one action"""
     env.reset()
     time_deb = time.perf_counter()
     env.policy()
     return time.perf_counter() - time_deb
 
 def measure_model_size(env):
+    """Count the number of parameters of the underlying neural network"""
     return sum(p.numel() for p in env.model.parameters())
 
-
 def evaluate_model_reward(env):
+    """Compute the average reward obtained by the model on the tracks he has never seen before"""
     sum = 0
     max_step = 300
     for i in range(env.track_for_training,len(TRACKS)) :
@@ -232,8 +234,20 @@ def evaluate_model_reward(env):
     return sum/( len(TRACKS) - env.track_for_training)
 
 def training(lr=1e-4,epsilon_decay=30.,batch_size = 40,time_bound = 60*(1),track_budget=int(0.8*len(TRACKS))):
+    """
+    We train a model initialised with random parameters using a Deep Q-Learning algorithm.
+    We allow to change 4 hyperparameters : 
+    - lr : the learning rate 
+    - epsilon_decay : measure the amount of random choices the model will do during the training
+    - time_bound : the time budget allowed for the training
+    - track_budget : the number tracks allowed for the training
+    We output a dictionnary containing informations on the training.
+    """
     env = Env()
     filename =  "saved_model/"  + datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    # Create the folder "saved_model" where the model's parameters will be saved
+    if not os.path.exists("saved_model"):
+        os.makedirs("saved_model")
 
     # track budget for training
     env.track_for_training = track_budget
@@ -275,35 +289,30 @@ def training(lr=1e-4,epsilon_decay=30.,batch_size = 40,time_bound = 60*(1),track
             else:
                 with torch.no_grad() :
                     action = env.policy()
-
-            # Apply the transition and save it in memory
+            # Apply the transition and save it in the memory
             transition = env.step(action)
             reward += (transition.reward).item()
             env.memory.push(transition)
-            # Optimize by observing batch_size random transitions
+            # Optimize the model according to batch_size random transitions
             optimize(env,optimizer,criterion,batch_size,discount_factor)
-        
+    
         # Stats about the training
         second = (int(time.perf_counter() - time_start)) % 60
         minute = (int(time.perf_counter() - time_start)) //60
+        window_len = 30
+        iteration_time = time.perf_counter() - time_start
         # We save the model every 5 minutes
         if (minute%5 == 0 and minute > 3) :
             env.model.save(filename)
-        normalizer = 1
-        window_len = 30
-        iteration_time = time.perf_counter() - time_start
         reward_time.append( iteration_time  )
         volatility_time.append(iteration_time)
-
-        reward_history.append(reward*normalizer)
-        
+        reward_history.append(reward)
         last_window = reward_history[-window_len:]
         volatility_history.append(np.std(last_window))
-        
     
     res = {}
     res["training_time"] = time_bound
-    res["track_number"] = len(TRACKS)
+    res["track_number"] = track_budget
     res["global_volatility"] = np.std(reward_history)
     res["model_size"] = measure_model_size(env)
     res["policy_time"] = measure_policy_time(env)
@@ -323,15 +332,12 @@ def training(lr=1e-4,epsilon_decay=30.,batch_size = 40,time_bound = 60*(1),track
     if (minute > 3) :
          env.model.save(filename)
          res["DQN_model_param_is_saved"] = True
-
-    # plt.plot(reward_history)
-    # plt.plot(volatility_history)
-    # plt.show()
     
     return res
 
 
 def playing(env):
+    """Observe how the model behave on all tracks"""
     sum = 0
     max_step = 300
     mini = 0
@@ -349,50 +355,62 @@ def playing(env):
 
 
 def part1_data():
+    """Compute the data to compare Deep Q-Learning with Genetic algorithms"""
     training_times_min = [10,40,60]
     training_times_sec = [60*i for i in training_times_min]
     track_limit = [8,40, int(0.8*len(TRACKS)) ]
+    # We check if we have enough tracks to do the training
     if (len(TRACKS) < max(track_limit)) :
         raise(ValueError("not enough tracks"))
+    # Create the folder associated with the training
     folder_name =  "run_part1_"+ datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     os.makedirs(folder_name)
-    for t in training_times_sec :
-        for n_track in track_limit :
-            json_object = json.dumps( training(time_bound=t,track_budget=n_track))
-            with open(folder_name + "/" + str(t)+"_"+str(n_track)+".json","w") as f :
+    # Train the model
+    for time_bound in training_times_sec :
+        for track_budget in track_limit :
+            json_object = json.dumps( training(time_bound=time_bound,track_budget=track_budget))
+            with open(folder_name + "/" + str(time_bound)+"_"+str(track_budget)+".json","w") as f :
                 f.write(json_object)
 
 def part2_data():
+    """Compute the data to evaluate the dependence of the training on Hyperparameters (learning rate, batch size and epsilon decay)"""
     training_time = 30*(60)
     lr_l = [1e-2,1e-3,1e-4,1e-5,1e-6]
     batch_size_l = [10,30,50,80,120]
     epsilon_decay_l = [10.,30.,50.,120.,200.]
-    track_limit = int(0.8*len(TRACKS))
+    track_budget = int(0.8*len(TRACKS))
 
+    # Create the folder associated with the training
     folder_name =  "run_part2_"+ datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     os.makedirs(folder_name)
 
-    for par in lr_l :
-        json_object = json.dumps( training(lr=par, track_budget=track_limit , time_bound=training_time ))
-        with open(folder_name + "/" + "lr"+"_"+str(par)+".json","w") as f :
+    # For learning rate
+    for lr in lr_l :
+        json_object = json.dumps( training(lr=lr, track_budget=track_budget , time_bound=training_time ))
+        with open(folder_name + "/" + "lr"+"_"+str(lr)+".json","w") as f :
             f.write(json_object)
 
-    for par in batch_size_l :
-        json_object = json.dumps( training(batch_size=par, track_budget=track_limit , time_bound=training_time ))
-        with open(folder_name + "/" + "batch_size"+"_"+str(par)+".json","w") as f :
+     # For batch size
+    for batch_size in batch_size_l :
+        json_object = json.dumps( training(batch_size=batch_size, track_budget=track_budget , time_bound=training_time ))
+        with open(folder_name + "/" + "batch_size"+"_"+str(batch_size)+".json","w") as f :
             f.write(json_object)
 
-    for par in epsilon_decay_l :
-        json_object = json.dumps( training(epsilon_decay=par, track_budget=track_limit , time_bound=training_time ))
-        with open(folder_name + "/" + "epsilon_decay"+"_"+str(par)+".json","w") as f :
+    # For epsilon decay
+    for epsilon_decay in epsilon_decay_l :
+        json_object = json.dumps( training(epsilon_decay=epsilon_decay, track_budget=track_budget , time_bound=training_time ))
+        with open(folder_name + "/" + "epsilon_decay"+"_"+str(epsilon_decay)+".json","w") as f :
             f.write(json_object)
 
 
 def part3_data():
+    "Compute the data associated with a training of six hours"
     training_times = (60)*(60)*6
     track_limit = int(0.8*len(TRACKS))
+    # Create the folder associated with the training
     folder_name =  "run_part3_"+ datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     os.makedirs(folder_name)
+    # Train the model
     json_object = json.dumps( training(time_bound=training_times,track_budget=track_limit, lr=1e-5 , epsilon_decay=500.))
     with open(folder_name + "/" + "long training"+".json","w") as f :
         f.write(json_object)
@@ -400,3 +418,4 @@ def part3_data():
 
 # part1/2/3_data are used to compute the data of the section Evaluation of the report
 
+training(time_bound=20)
